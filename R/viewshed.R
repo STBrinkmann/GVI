296fad254ef91257cd807d5c191448183a280afd
STBrinkmann <bastibrinkmann94@gmail.com>
2022-03-26 15:26:22 +0100

    Remove cells outside buffer

--- R/viewshed.R
+++ R/viewshed.R
@@ -143,6 +143,7 @@ viewshed <- function(observer, dsm_rast, dtm_rast,
   
   #### 4. Compare DSM with Visibility ####
   if (plot) {
+    dsm_rast_masked[which(!(1:ncell(output)) %in% c(cell_0, (cells_in_vs+1)))] <- NA
     graphics::par(mfrow=c(1,2))
     terra::plot(dsm_rast_masked, legend = F); graphics::points(x0, y0, col = "red", pch = 20, cex = 2)
     terra::plot(output, legend = F); graphics::points(x0, y0, col = "red", pch = 20, cex = 2)
